6794965c5b84e231784cd43e50a0895ebe000f70
Toan <tpkhuong@gmail.com>
2021-02-24 07:19:22 -0500

    js code not swipe feature

--- newbie/codingBootcampSlider/ourApp.js
+++ newbie/codingBootcampSlider/ourApp.js
@@ -1,25 +1,37 @@
 function ourSelectors() {
-  var sectionElementContentContainer = document.querySelector(".testimonial");
+  var sectionElementContentContainers = document.querySelectorAll(
+    ".testimonial"
+  );
 
   return {
-    sectionElementContentContainer,
+    sectionElementContentContainers,
   };
 }
 
 touchFuntionality();
 
 function touchFuntionality(
-  { sectionElementContentContainer } = ourSelectors()
+  { sectionElementContentContainers } = ourSelectors()
 ) {
-  var strOfEventListern = ["touchstart", "touchmove", "touchend"];
+  var strOfEventListener = ["touchstart", "touchmove", "touchend"];
   /***** look at event object key/property changedTouches(an array) access that array look at pageX and pageY. there is always clientX and clientY
    * and screenX and screenY
    *  *****/
 
+  var arrOfTestimonialsContainers = Array.from(sectionElementContentContainers);
+  arrOfTestimonialsContainers.forEach(function listenForTouch(eachTestimonial) {
+    eachTestimonial.addEventListener(
+      "touchstart",
+      function clickingOnTestimonial(event) {
+        console.dir(event.target);
+      }
+    );
+  });
+
   /***** pageY clicking in higher area will have a lower number than clicking in lower area *****/
-  var strOfEventListern = ["touchstart", "touchend"];
-  strOfEventListern.forEach(function addEventListener(eachStr) {
-    sectionElementContentContainer.addEventListener(
+  var strOfEventListener = ["touchstart", "touchend"];
+  strOfEventListener.forEach(function addEventListener(eachStr) {
+    sectionElementContentContainers.addEventListener(
       `${eachStr}`,
       function getPositionOnScreen(event) {
         console.log(event);
