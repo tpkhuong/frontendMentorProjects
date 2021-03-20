5d9febe6ff4e2dfd43a766d3b751c33a1c89fca8
Toan <tpkhuong@gmail.com>
2021-03-20 06:39:56 -0400

    js code active state and hover state mobile js

--- newbie/pingSingleColumn/ourApp.js
+++ newbie/pingSingleColumn/ourApp.js
@@ -13,7 +13,17 @@ errorMessage();
 function errorMessage({ formElement, emailInput } = ourSelectors()) {
   formElement.addEventListener("submit", function applyErrorMessage(event) {
     event.preventDefault();
-    alert("work on error message");
-    console.log(emailInput);
+    var spanElement = emailInput.nextElementSibling;
+    if (!emailInput.validity.valid) {
+      emailInput.classList.add("invalid-message-ring");
+      spanElement.classList.remove("hide-off-left");
+    } else {
+      emailInput.classList.remove("invalid-message-ring");
+      spanElement.classList.add("hide-off-left");
+    }
+  });
+
+  window.addEventListener("load", function clearInput(event) {
+    emailInput.value = "";
   });
 }
