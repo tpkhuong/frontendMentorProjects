3fd861646ffd6ca44f30288861405d7c51cb5201
Toan <tpkhuong@gmail.com>
2021-03-26 06:29:28 -0400

    hamburger button works js

--- intermediate/blogrLandingPage/ourApp.js
+++ intermediate/blogrLandingPage/ourApp.js
@@ -12,4 +12,20 @@ function toggleNavMenu({ hamburgerElement } = ourSelectors()) {
   hamburgerElement.addEventListener("click", function addClass(event) {
     this.classList.toggle("open");
   });
+  hamburgerElement.addEventListener(
+    "keydown",
+    function keyboardFunctionality(event) {
+      event.preventDefault();
+      console.log(event);
+      var keyPressed = event.code;
+      switch (keyPressed) {
+        case "Space":
+          this.classList.toggle("open");
+          break;
+        case "Enter":
+          this.classList.toggle("open");
+          break;
+      }
+    }
+  );
 }
