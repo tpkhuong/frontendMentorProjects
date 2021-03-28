42bf4cede6800e0d95341f322cbba7c9d8e95a30
Toan <tpkhuong@gmail.com>
2021-03-28 06:51:53 -0400

    fixed outline for button js

--- intermediate/blogrLandingPage/ourApp.js
+++ intermediate/blogrLandingPage/ourApp.js
@@ -1,31 +1,46 @@
 function ourSelectors() {
   var hamburgerElement = document.querySelector(".hamburger");
+  var navContainer = document.querySelector(".nav-login-container");
+  var headerNav = document.querySelector(".header-nav");
 
   return {
     hamburgerElement,
+    navContainer,
+    headerNav,
   };
 }
 
 toggleNavMenu();
 
-function toggleNavMenu({ hamburgerElement } = ourSelectors()) {
+function toggleNavMenu({ hamburgerElement, navContainer } = ourSelectors()) {
   hamburgerElement.addEventListener("click", function addClass(event) {
     this.classList.toggle("open");
+    navContainer.classList.toggle("hide-off-left");
   });
   hamburgerElement.addEventListener(
     "keydown",
     function keyboardFunctionality(event) {
-      event.preventDefault();
       console.log(event);
       var keyPressed = event.code;
       switch (keyPressed) {
         case "Space":
+          event.preventDefault();
           this.classList.toggle("open");
+          navContainer.classList.toggle("hide-off-left");
           break;
         case "Enter":
           this.classList.toggle("open");
+          navContainer.classList.toggle("hide-off-left");
           break;
       }
     }
   );
+}
+
+showSubmenu();
+
+function showSubmenu({ headerNav } = ourSelectors()) {
+  headerNav.addEventListener("keydown", function toggleSubmenu(event) {
+    console.log(event.target);
+  });
 }
