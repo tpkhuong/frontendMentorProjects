2f6f4a297427c1ca6df27c713d9cedfc8043b852
Toan <tpkhuong@gmail.com>
2021-03-31 06:33:12 -0400

    hover works js

--- intermediate/blogrLandingPage/ourApp.js
+++ intermediate/blogrLandingPage/ourApp.js
@@ -3,7 +3,7 @@ function ourSelectors() {
   var navContainer = document.querySelector(".nav-login-container");
   var headerNav = document.querySelector(".header-nav");
   var btnElements = Array.from(
-    document.querySelectorAll("button.btn-link[aria-expanded]")
+    document.querySelectorAll("a.btn-link[aria-expanded]")
   );
 
   return {
@@ -45,12 +45,20 @@ function toggleNavMenu(
 }
 
 showSubmenu();
+keyboardFunctionality();
 
 function showSubmenu({ headerNav, btnElements } = ourSelectors()) {
   headerNav.addEventListener("keydown", function toggleSubmenu(event) {
     var [productBtn, companyBtn, connectBtn] = btnElements;
     var keyPressed = event.code;
     if (keyPressed == "Space") {
+      event.preventDefault();
+      let arrOfSubmenuElements = Array.from(
+        event.target.nextElementSibling.children
+      );
+      var [firstLiElementSubmenu] = arrOfSubmenuElements;
+      firstLiElementSubmenu.firstElementChild.focus();
+
       var strTextOfBtnElement = event.target.innerText;
       switch (strTextOfBtnElement) {
         case "Product":
@@ -60,11 +68,13 @@ function showSubmenu({ headerNav, btnElements } = ourSelectors()) {
           connectBtn.attributes["aria-expanded"].value = false;
           break;
         case "Company":
+          // firstLiElementSubmenu.focus();
           productBtn.attributes["aria-expanded"].value = false;
           companyBtn.attributes["aria-expanded"].value = true;
           connectBtn.attributes["aria-expanded"].value = false;
           break;
         case "Connect":
+          // firstLiElementSubmenu.focus();
           productBtn.attributes["aria-expanded"].value = false;
           companyBtn.attributes["aria-expanded"].value = false;
           connectBtn.attributes["aria-expanded"].value = true;
@@ -72,6 +82,13 @@ function showSubmenu({ headerNav, btnElements } = ourSelectors()) {
       }
     }
     if (keyPressed == "Enter") {
+      event.preventDefault();
+      let arrOfSubmenuElements = Array.from(
+        event.target.nextElementSibling.children
+      );
+      var [firstLiElementSubmenu] = arrOfSubmenuElements;
+      firstLiElementSubmenu.firstElementChild.focus();
+
       var strTextOfBtnElement = event.target.innerText;
       switch (strTextOfBtnElement) {
         case "Product":
@@ -170,4 +187,60 @@ function showSubmenu({ headerNav, btnElements } = ourSelectors()) {
       }
     }
   });
+  headerNav.addEventListener("mouseover", function toggleSubmenu(event) {
+    // var arrOfSubmenuElements = Array.from(
+    //   event.target.nextElementSibling.children
+    // );
+    // var [firstSubmenuElement] = arrOfSubmenuElements;
+    // firstSubmenuElement.firstElementChild.focus();
+    var [productBtn, companyBtn, connectBtn] = btnElements;
+    var btnElementInnerText = event.target.innerText;
+    if (event.target.className.includes("btn-link")) {
+      switch (btnElementInnerText) {
+        case "Product":
+          productBtn.attributes["aria-expanded"].value = true;
+          companyBtn.attributes["aria-expanded"].value = false;
+          connectBtn.attributes["aria-expanded"].value = false;
+          break;
+        case "Company":
+          productBtn.attributes["aria-expanded"].value = false;
+          companyBtn.attributes["aria-expanded"].value = true;
+          connectBtn.attributes["aria-expanded"].value = false;
+          break;
+        case "Connect":
+          productBtn.attributes["aria-expanded"].value = false;
+          companyBtn.attributes["aria-expanded"].value = false;
+          connectBtn.attributes["aria-expanded"].value = true;
+          break;
+      }
+    }
+  });
 }
+
+function keyboardFunctionality({ headerNav, btnElements } = ourSelectors()) {}
+
+/*
+
+// Replace this with a relevant selector.
+// If you use a tool that auto-generates classes,
+// you can temporarily add an ID and select it
+// with '#id'.
+const selector = '.the-fixed-child';
+function findCulprits(elem) {
+  if (!elem) {
+    throw new Error(
+      'Could not find element with that selector'
+    );
+  }
+  let parent = elem.parentElement;
+  while (parent) {
+    const hasOverflow = getComputedStyle(parent).overflow;
+    if (hasOverflow !== 'visible') {
+      console.log(hasOverflow, parent);
+    }
+    parent = parent.parentElement;
+  }
+}
+findCulprits(document.querySelector(selector));
+
+*/
