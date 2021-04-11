62cc22d4c21c460efdb068997ec3b27b94db4117
Toan <tpkhuong@gmail.com>
2021-04-11 06:51:09 -0400

    ATL a11y helper functions

--- intermediate/blogrLandingPage/ourApp.js
+++ intermediate/blogrLandingPage/ourApp.js
@@ -736,3 +736,79 @@ function findCulprits(elem) {
 findCulprits(document.querySelector(selector));
 
 */
+
+/***** check img alt text *****/
+var imgArr = Array.from(document.querySelectorAll("img"));
+console.log(imgArr);
+console.log(`this is the array of imgs: ${imgArr}`);
+imgArr.forEach(function printAltText(eachImg) {
+  // console.dir(eachImg);
+  if (eachImg.alt === "") {
+    console.dir(`this is empty alt text: ${eachImg}`);
+  } else {
+    console.log(`this has alt text: ${eachImg.alt}`);
+    console.dir(eachImg);
+  }
+});
+
+/***** check img alt text *****/
+
+/***** select all element in the body to check fontsize *****/
+
+function findParaElements(classOrIdOfMainElement) {
+  var allParagraph = Array.from(
+    document.querySelectorAll(`#${classOrIdOfMainElement} p`)
+  );
+  allParagraph.forEach(function printFont(eachElement) {
+    console.log(getComputedStyle(eachElement).fontSize);
+  });
+  console.log(allParagraph);
+  console.log(`#${classOrIdOfMainElement} p`);
+}
+
+/***** get headingss *****/
+
+function getAllHeadings() {
+  var headingH1 = document.querySelectorAll("h1");
+  var headingH2 = document.querySelectorAll("h2");
+  var headingH3 = document.querySelectorAll("h3");
+  var headingH4 = document.querySelectorAll("h4");
+  var headingH5 = document.querySelectorAll("h5");
+  var headingH6 = document.querySelectorAll("h6");
+
+  var printHeadings = [
+    headingH1,
+    headingH2,
+    headingH3,
+    headingH4,
+    headingH5,
+    headingH6,
+  ];
+
+  printHeadings.forEach(function consoleHeadings(eachHeading) {
+    console.log(eachHeading);
+  });
+}
+
+/***** get headingss *****/
+
+var allELementsOfBodyEle = Array.from(document.querySelectorAll("body *"));
+
+allELementsOfBodyEle.forEach(function findFontSize(eachElement) {
+  console.log(getComputedStyle(eachElement).fontSize);
+  var fontSizeOfElement = Number(
+    getComputedStyle(eachElement).fontSize.split("p")[0]
+  );
+
+  if (fontSizeOfElement < 16) {
+    console.dir(fontSizeOfElement);
+  }
+});
+
+var allParagraphs = Array.from(document.querySelectorAll("body p"));
+
+allParagraphs.forEach(function printFont(eachPara) {
+  console.log(getComputedStyle(eachPara).fontSize);
+});
+
+/***** select all element in the body to check fontsize *****/
