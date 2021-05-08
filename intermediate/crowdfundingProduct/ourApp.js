2ed935ada68f0374c86645f9a4f7d6eb9e97bd3b
Toan <tpkhuong@gmail.com>
2021-05-08 06:50:58 -0400

    working on faded effect js

--- intermediate/crowdfundingProduct/ourApp.js
+++ intermediate/crowdfundingProduct/ourApp.js
@@ -1,13 +1,19 @@
 function ourSelectors() {
   var navBar = document.querySelector(".nav-list");
-
+  // var quantitySelectors = Array.prototype.slice.call(
+  //   document.querySelectorAll(".quantity-digit")
+  // );
+  var quantitySelectors = Array.from(
+    document.querySelectorAll(".quantity-digit")
+  );
   return {
     navBar,
+    quantitySelectors,
   };
 }
 
 toggleNavMenu();
-
+addFadedOpacity();
 function toggleNavMenu() {
   var { navBar } = ourSelectors();
   var toggleButton = document.querySelector(".headernav button.img-container");
@@ -15,4 +21,21 @@ function toggleNavMenu() {
   toggleButton.addEventListener("click", function showMenu(event) {
     navBar.classList.toggle("hide-off-left");
   });
+}
+
+function addFadedOpacity() {
+  var { quantitySelectors } = ourSelectors();
+
+  var quantityIsZero = quantitySelectors.reduce(function findZeroLeft(
+    buildingUp,
+    currentValue
+  ) {
+    var converToNum = Number(currentValue.innerText);
+    if (converToNum === 0) {
+      return [...buildingUp, currentValue];
+    }
+    return buildingUp;
+  },
+  []);
+  console.log(quantityIsZero);
 }
