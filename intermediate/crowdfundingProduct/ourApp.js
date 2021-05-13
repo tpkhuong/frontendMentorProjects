0a104a5d82535c616c759113b65d3221ffbfe10f
Toan <tpkhuong@gmail.com>
2021-05-13 07:24:54 -0400

    selecting the quantity digit making progress js

--- intermediate/crowdfundingProduct/ourApp.js
+++ intermediate/crowdfundingProduct/ourApp.js
@@ -23,6 +23,17 @@ toggleNavMenu();
 addFadedOpacity();
 selectedBookmarked();
 inputFunctionality();
+// ourData();
+// onlyRunInputFuncWhenBtnClicked();
+
+function ourData(dataInput = {}) {
+  var dataObj = {};
+  var [ourArr] = Object.entries(dataInput);
+  var [ourKey, ourValue] = ourArr;
+  // dataObj[ourKey] = ourValue;
+  dataObj[ourKey] = ourValue;
+  console.log(dataObj);
+}
 
 function toggleNavMenu() {
   var { navBar } = ourSelectors();
@@ -84,22 +95,28 @@ function selectedBookmarked() {
       this.classList.toggle("activated-bookmarked");
     });
 }
-alert("add submit btn class to continue btn");
+
+/* just add btn functionality when the submit button is clicked*/
+
 function inputFunctionality() {
   var { selectPledgeInput } = ourSelectors();
-  var submitBtn = document.querySelector(".submit-btn");
+  // var submitBtn = Array.prototype.slice.call( document.querySelectorAll(".submit-btn"));
+  var submitBtn = Array.from(document.querySelectorAll(".submit-btn"));
 
-  submitBtn.addEventListener("click", function getInputValue(event) {
-    // var getValueOfInputEle = Array.prototype.slice.call(
-    //   event.target.previousElementSibling.children
-    // );
-    var [getValueOfInputEle] = Array.from(
-      event.target.previousElementSibling.children
-    ).filter(function findMatchingClass(eachChildELe) {
-      return eachChildELe.className == "selected-pledge-input";
+  submitBtn.forEach(function addEventToBtn(eachBtn) {
+    eachBtn.addEventListener("click", function getInputValue(event) {
+      // var getValueOfInputEle = Array.prototype.slice.call(
+      //   event.target.previousElementSibling.children
+      // );
+      var [getValueOfInputEle] = Array.from(
+        event.target.previousElementSibling.children
+      ).filter(function findMatchingClass(eachChildELe) {
+        return eachChildELe.className == "selected-pledge-input";
+      });
+      /**** we are getting the value of our inputs when we click the continue btn *****/
+      ourData(ourAmount(Number(getValueOfInputEle.value)));
+      // console.log(ourAmount(Number(getValueOfInputEle.value)));
     });
-    /**** we are getting the value of our inputs when we click the continue btn *****/
-    console.log(Number(getValueOfInputEle.value));
   });
   // selectPledgeInput.forEach(function onlyRunWhenFocused(eachInput) {
   //   eachInput.addEventListener(
@@ -112,4 +129,108 @@ function inputFunctionality() {
   //     }
   //   );
   // });
+}
+
+function ourAmount(amountInput) {
+  return { amountInput };
+}
+/* just add btn functionality when the submit button is clicked*/
+
+function testedCode() {
+  function onlyRunInputFuncWhenBtnClicked() {
+    var arrOfSubmitBtn = Array.from(document.querySelectorAll(".submit-btn"));
+
+    arrOfSubmitBtn.forEach(function addClickEvent(eachBtn) {
+      eachBtn.addEventListener("click", inputFunctionality);
+    });
+  }
+}
+
+findRadioBtnChangeEvent();
+function findRadioBtnChangeEvent() {
+  // var inputRadioBtn = Array.from(
+  //   document.querySelectorAll("input[type='radio']")
+  // );
+  var inputRadioBtnArray = Array.prototype.slice.call(
+    document.querySelectorAll("input[type='radio']")
+  );
+
+  var addListenerToDialog1 = document.querySelector('[id="dialog1"]');
+
+  addListenerToDialog1.addEventListener(
+    "change",
+    function watchForChange(event) {
+      var radioBtnHasCheckedTrue = event.target;
+      console.log(radioBtnHasCheckedTrue);
+      /* turn the radio btn we clicked aria checked to true*/
+      event.target.attributes["aria-checked"].value = "true";
+      toggleAriaChecked(radioBtnHasCheckedTrue, inputRadioBtnArray);
+      findTheQuantityOfPledge(radioBtnHasCheckedTrue);
+    }
+  );
+  // inputRadioBtnArray.forEach(function addChangeListener(eachRadioBtn) {
+  //   eachRadioBtn.addEventListener("change", function watchForChange(event) {
+  //     var radioBtnHasCheckedTrue = event.target;
+  //     /* turn the radio btn we clicked aria checked to true*/
+  //     event.target.attributes["aria-checked"].value = "true";
+  //     toggleAriaChecked(radioBtnHasCheckedTrue, inputRadioBtnArray);
+  //     findTheQuantityOfPledge(radioBtnHasCheckedTrue);
+  //   });
+  // });
+  // inputRadioBtn.forEach(function printStuff(eachRadioBtn) {
+  //   console.log(eachRadioBtn.checked);
+  // });
+}
+
+function toggleAriaChecked(radioBtnInput, arrRadioBtn) {
+  var notCheckedTurnAriaToFalse = arrRadioBtn.filter(function findFalse(
+    eachBtn
+  ) {
+    return eachBtn != radioBtnInput;
+  });
+
+  notCheckedTurnAriaToFalse.forEach(function turnAriaCheckedFalse(
+    eachRadioBtn
+  ) {
+    eachRadioBtn.attributes["aria-checked"].value = "false";
+  });
+}
+alert(
+  "we are selecting the text of the quantity-digit element. might be better to put unique identifier for our quantity-digit element"
+);
+function findTheQuantityOfPledge(selectedRadioBtn) {
+  var selectedRadioLabelText = selectedRadioBtn.nextElementSibling.innerText;
+  console.log(selectedRadioLabelText);
+
+  if (!selectedRadioLabelText.includes("Pledge")) {
+    var arrOfIndividualPledgeEle = Array.prototype.slice.call(
+      document.querySelectorAll(".individual-pledge")
+    );
+
+    console.log(arrOfIndividualPledgeEle);
+
+    var [articleEleQuantityToChange] = arrOfIndividualPledgeEle.filter(
+      function printInnerText(eachArticle) {
+        var titleEleInnerText =
+          eachArticle.firstElementChild.firstElementChild.innerText;
+
+        return selectedRadioLabelText == titleEleInnerText;
+      }
+    );
+    // amount-pledges-left-select
+    var childrenOfArticleEle = Array.prototype.slice.call(
+      articleEleQuantityToChange.children
+    );
+    // var childrenOfArticleEle = Array.from(articleEleQuantityToChange);
+
+    var [containerOfQuantityDigit] = childrenOfArticleEle.filter(
+      function findTheContainer(eachElement) {
+        return eachElement.className == "amount-pledges-left-select";
+      }
+    );
+
+    console.log(
+      containerOfQuantityDigit.firstElementChild.firstElementChild.innerText
+    );
+  }
 }
