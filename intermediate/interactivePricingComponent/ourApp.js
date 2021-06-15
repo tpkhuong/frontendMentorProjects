c834b83c252f6286dd31d8a2ee046f537febc2c4
Toan <tpkhuong@gmail.com>
2021-06-15 06:34:31 -0400

    working on slider feature js

--- intermediate/interactivePricingComponent/ourApp.js
+++ intermediate/interactivePricingComponent/ourApp.js
@@ -115,14 +115,51 @@ function returnMonthOrYearDataObj(sliderPosition) {
 /***** our data will be selected based on the slider position *****/
 testingIdeas();
 function testingIdeas() {
+  document.documentElement.style.setProperty(
+    "--slider-movement",
+    "0px"
+    // String(totalAmtPercentage) + "%"
+  );
   var sliderContainer = document.querySelector(".slider");
   // use mousedown them mousemove then mouseup
   console.log(sliderContainer.firstElementChild.nextElementSibling);
   sliderContainer.addEventListener(
     "mousedown",
     function watchMovingSlider(event) {
+      console.log(event);
       console.log(event.pageX);
+      console.log(event.target);
+      // if (event.target == this) {
+      //   this.addEventListener("mousemove", function testingAlgorithm(event) {
+      //     console.log(event.pageX);
+      //   });
+      // }
+      // var pixelMoveMnt = event.pageX;
+      // console.log(`${pixelMoveMnt}`);
+      // console.log(typeof pixelMoveMnt);
+      // document.documentElement.attributes["style"].value =
+      //   "--slider-movement: " + String(pixelMoveMnt) + "px";
     }
   );
+  alert("our slider-icon-wrapper is moving based on movement of mouse");
+  alert(
+    "we will figure out how to add one or substract one pixel based on mouse horizontal movement"
+  );
+  alert(
+    "get pageX divide by pageX add it to a variable use that variable to change --slider-movement"
+  );
+  // sliderContainer.addEventListener(
+  //   "mousemove",
+  //   function watchMovingSlider(event) {
+  //     console.log(event);
+  //     console.log(event.pageX);
+  //     var pixelMoveMnt = event.pageX;
+  //     console.log(`${pixelMoveMnt}`);
+  //     console.log(typeof pixelMoveMnt);
+  //     document.documentElement.attributes["style"].value =
+  //       "--slider-movement: " + String(pixelMoveMnt) + "px";
+  //   }
+  // );
+  // alert("our slider-icon-wrapper is moving based on movement of mouse")
   //the slider element transform: translateX() will be based on the mousemove pageX position
 }
