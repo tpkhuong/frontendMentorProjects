5f789c4aa51ff354ed98292c16a4fc9745dc4569
Toan <tpkhuong@gmail.com>
2021-07-21 06:28:32 -0400

    working on display mobile navbar menu js

--- intermediate/loopstudiosLandingPage/ourApp.js
+++ intermediate/loopstudiosLandingPage/ourApp.js
@@ -1,1 +1,54 @@
-alert("use document.activeElement for modal/mobile menu");
+(function callOurApp() {
+  function ourSelectors() {
+    //menu button
+    var menuBtn = document.querySelector(".menu-button");
+    //close button
+    var closeBtn = document.querySelector(".close-button");
+    return {
+      menuBtn,
+      closeBtn,
+    };
+  }
+
+  //call our functions
+  openMobileMenu();
+  closeMobileMenu();
+  function openMobileMenu() {
+    var { menuBtn, closeBtn } = ourSelectors();
+    var menuBtnSvg = document.getElementById("mobile-menu-btn-img");
+    menuBtn.addEventListener("click", showMobileNavbar);
+
+    function showMobileNavbar(event) {
+      if (
+        event.target === menuBtn ||
+        event.target === menuBtnSvg ||
+        event.target.parentElement === menuBtnSvg ||
+        event.target.parentElement.parentElement === menuBtnSvg
+      ) {
+        //when use click on menu btn change btn aria-pressed to "true"
+        menuBtn.attributes["aria-pressed"].value = "true";
+        setTimeout(function focusFirstElementOfModal() {
+          closeBtn.focus();
+        }, 300);
+      }
+    }
+  }
+
+  function closeMobileMenu() {
+    var { menuBtn, closeBtn } = ourSelectors();
+    var closeBtnSvg = document.getElementById("mobile-menu-closed-img");
+
+    closeBtn.addEventListener("click", hideMobileNavbar);
+
+    function hideMobileNavbar(event) {
+      if (
+        event.target === closeBtn ||
+        event.target === closeBtnSvg ||
+        event.target.parentElement === closeBtnSvg ||
+        event.target.parentElement.parentElement === closeBtnSvg
+      ) {
+        menuBtn.attributes["aria-pressed"].value = "false";
+      }
+    }
+  }
+})();
