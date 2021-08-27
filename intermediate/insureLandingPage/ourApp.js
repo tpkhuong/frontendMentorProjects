543fea43d7d783f8442f0906f6d4f760606b21ce
Toan <tpkhuong@gmail.com>
2021-08-27 06:38:53 -0400

    hovering effect completed js

--- intermediate/insureLandingPage/ourApp.js
+++ intermediate/insureLandingPage/ourApp.js
@@ -1,3 +1,46 @@
 (function scopeOurVariables() {
-  console.log("work on hover effect and navbar menu open/close feature");
+  //declare our selectors at top
+  const { openNavbarButton, closeNavbarButton, navbarContainer } =
+    ourSelectors();
+
+  function ourSelectors() {
+    //open navbar menu button
+    var openNavbarButton = document.querySelector(".open-button");
+    //close navbar menu button
+    var closeNavbarButton = document.querySelector(".close-button");
+    //article navbar
+    var navbarContainer = document.querySelector(".navbar");
+    return {
+      openNavbarButton,
+      closeNavbarButton,
+      navbarContainer,
+    };
+  }
+
+  //call our funcs
+  addEventListenerToElement();
+  function addEventListenerToElement() {
+    navbarContainer.addEventListener("touchstart", toggleNavMenu);
+  }
+
+  function toggleNavMenu(event) {
+    var parentElementClass = event.target.parentElement.className.split("-")[0];
+
+    if (parentElementClass == "open" || event.target == openNavMenu) {
+      openNavMenu(event);
+    }
+
+    if (parentElementClass == "close" || event.target == closeNavMenu) {
+      closeNavMenu(event);
+    }
+  }
+
+  function openNavMenu(eventInput) {
+    openNavbarButton.attributes["aria-pressed"].value = "true";
+    closeNavbarButton.attributes["aria-pressed"].value = "false";
+  }
+
+  function closeNavMenu(eventInput) {
+    console.log(eventInput);
+  }
 })();
