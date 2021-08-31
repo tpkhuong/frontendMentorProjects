523b3df14134c4d35d21da920de30e7587e79363
Toan <tpkhuong@gmail.com>
2021-08-31 06:27:58 -0400

    tab order modal and focus on close/open button complete

--- intermediate/insureLandingPage/ourApp.js
+++ intermediate/insureLandingPage/ourApp.js
@@ -1,8 +1,11 @@
 (function scopeOurVariables() {
   //declare our selectors at top
-  alert("make navbar tab work with keyboard");
-  const { openNavbarButton, closeNavbarButton, navbarContainer } =
-    ourSelectors();
+  const {
+    openNavbarButton,
+    closeNavbarButton,
+    navbarContainer,
+    navbarBtnLastItem,
+  } = ourSelectors();
 
   function ourSelectors() {
     //open navbar menu button
@@ -11,10 +14,13 @@
     var closeNavbarButton = document.querySelector(".close-button");
     //article navbar
     var navbarContainer = document.querySelector(".navbar");
+    //last item in modal
+    var navbarBtnLastItem = document.querySelector(".navbar-btn");
     return {
       openNavbarButton,
       closeNavbarButton,
       navbarContainer,
+      navbarBtnLastItem,
     };
   }
 
@@ -22,6 +28,7 @@
   addEventListenerToElement();
   function addEventListenerToElement() {
     navbarContainer.addEventListener("touchstart", toggleNavMenu);
+    navbarContainer.addEventListener("keydown", tabOrderModal);
   }
 
   function toggleNavMenu(event) {
@@ -39,10 +46,42 @@
   function openNavMenu(eventInput) {
     openNavbarButton.attributes["aria-pressed"].value = "true";
     closeNavbarButton.attributes["aria-pressed"].value = "false";
+    // without eventInput.preventDefault() browser did not focus closeNavbarButton
+    //after 500ms of clicking on openNavbarButton
+    eventInput.preventDefault();
+    setTimeout(function focusCloseBtn() {
+      closeNavbarButton.focus();
+    }, 500);
   }
 
   function closeNavMenu(eventInput) {
     closeNavbarButton.attributes["aria-pressed"].value = "true";
     openNavbarButton.attributes["aria-pressed"].value = "false";
+    // without eventInput.preventDefault() browser did not focus openNavbarButton
+    //after 500ms of clicking on closeNavbarButton
+    eventInput.preventDefault();
+    setTimeout(function focusCloseBtn() {
+      openNavbarButton.focus();
+    }, 500);
+  }
+
+  function tabOrderModal(eventInput) {
+    if (eventInput.shiftKey) {
+      // if (eventInput.key == "Tab" && eventInput.target == closeNavbarButton) {
+      //   navbarBtnLastItem.focus();
+      //   eventInput.preventDefault();
+      // }
+      // if we did not have event.preventDefault() when we hit shift and
+      // hit tab on closeNavbarButton in navbar the focus will be on the item before mavbarLastItem
+
+      eventInput.key == "Tab" && eventInput.target == closeNavbarButton
+        ? (navbarBtnLastItem.focus(), eventInput.preventDefault())
+        : null;
+    } else {
+      // if we did not have event.preventDefault() when we hit tab on lastItem in navbar the focus will be on the next item after closeNavbarButton
+      eventInput.target == navbarBtnLastItem
+        ? (closeNavbarButton.focus(), eventInput.preventDefault())
+        : null;
+    }
   }
 })();
