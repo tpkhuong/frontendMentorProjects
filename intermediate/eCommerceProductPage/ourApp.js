e966b475d62277a739ac9ca22244f5b73e6ce7c9
Toan <tpkhuong@gmail.com>
2021-11-15 04:16:15 -0500

    working on modal tab button keydown algorithm js

--- intermediate/eCommerceProductPage/ourApp.js
+++ intermediate/eCommerceProductPage/ourApp.js
@@ -1,23 +1,41 @@
 (function scopeOurVariables() {
   //   our selectors
-  const { mobileCloseBtn, lastItemOfMobileNav } = ourSelectors();
-
-  console.log(lastItemOfMobileNav);
-
+  const { headerElement, mobileCloseBtn, arrOfMobileNavbarAnchorTags } =
+    ourSelectors();
+  // add event listeners
+  addEventListeners();
+  console.log(headerElement);
   function ourSelectors() {
     const mobileCloseBtn = document.querySelector(
       "#mobile-modal-style-wrapper .close-menu-btn"
     );
     /** select the anchor tag of mobile nav menu **/
-    const lastItemOfMobileNav = document.querySelectorAll(
+    const arrOfMobileNavbarAnchorTags = document.querySelectorAll(
       ".mobile-menu-navigation .navlink"
     );
+    //   header element
+    const headerElement = document.querySelector("[role='banner']");
     return {
+      headerElement,
       mobileCloseBtn,
-      lastItemOfMobileNav,
+      arrOfMobileNavbarAnchorTags,
     };
   }
 
-  alert("start here get last item of array");
-  function getLastElementOfMobileNav(arrInput) {}
+  function addEventListeners() {
+    headerElement.addEventListener("keydown", keyboardFunctionality);
+  }
+
+  function keyboardFunctionality(event) {
+    //   check for shift key pressed before tab key pressed
+    if (document.activeElement == mobileCloseBtn) {
+      console.log("here");
+    }
+  }
+
+  function getLastElementOfMobileNav(arrInput) {
+    const lengthOfArrInput = arrInput.length;
+    const lastItemOfArr = arrInput[lengthOfArrInput - 1];
+    return lastItemOfArr;
+  }
 })();
