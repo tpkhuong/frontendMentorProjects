32a86403a374e2b69a3325b1890cf1ad7714e690
Toan <tpkhuong@gmail.com>
2021-12-19 04:16:00 -0500

    love the effort/process building countdown algorithm js

--- intermediate/launchCountdownTimer/ourApp.js
+++ intermediate/launchCountdownTimer/ourApp.js
@@ -24,6 +24,52 @@
       booleanShiftKeyPressed: this.shiftKey,
     };
   }
+  // countDown();
+  function countDown() {
+    setInterval(() => {
+      const currentDate = new Date();
+      let currentSecond = 60 - currentDate.getSeconds();
+      if (currentSecond == 60) {
+        currentSecond = 0;
+      }
+      console.log(currentSecond);
+    }, 1000);
+  }
+
+  const accessOurData = scopedOurData();
+
+  function scopedOurData() {
+    const innerDataObj = {
+      numOfDaysOfMonths: {
+        Jan: 31,
+        Feb: 28,
+        Mar: 31,
+        Apr: 30,
+        May: 31,
+        Jun: 30,
+        Jul: 31,
+        Aug: 31,
+        Sep: 30,
+        Oct: 31,
+        Nov: 30,
+        Dec: 31,
+      },
+      name: "Deadpool",
+      sayHi() {
+        console.log(`${this.name} hello`);
+      },
+    };
+
+    return function closureOverOurVariables() {
+      return innerDataObj;
+    };
+  }
+
+  const dataObj = accessOurData();
+  const testObj = {
+    name: "Spider-Man",
+  };
+  console.log(dataObj.sayHi.call(testObj));
 
   function ourSelectors() {
     const effortTwo = document.querySelector("digit-style-wrapper-two");
