aed89012947cec8beee237363e4118b745ac056e
Toan <tpkhuong@gmail.com>
2021-12-21 04:17:58 -0500

    love the effort/process building accessible countdown timer js

--- intermediate/launchCountdownTimer/ourApp.js
+++ intermediate/launchCountdownTimer/ourApp.js
@@ -78,4 +78,48 @@
       effortTwo,
     };
   }
+
+  function findIndexOfSubarray(arrInput, strFormOfMonth) {}
+
+  /***
+   * ex: Dec 20, 2021 starting date March 23,2022
+   * get num of days of current month which will be 31
+   * call getDate() on currentDate which will be 20
+   * subtract date of currentDate 20 from num of day of current month 31
+   * we will get 11
+   * ***/
+  /***
+   * months between starting date month and current date month
+   * work with the index. have a starting index and ending index
+   * we will get index of current month(starting index) and index of starting date month(ending index)
+   * december is 11(starting) march is 2(ending)
+   * first effort
+   * let index = 0;
+   * let counter = 0;
+   * 
+   * while (true) {
+    console.log(arrayOfSubarrays[index]);
+    if (index == 11) {
+      index = 0;
+      counter++
+    } else {
+      index++;
+    }
+    if (counter == 2) return;
+  }
+   * ***/
+  const arrayOfSubarrays = Object.entries(dataObj.numOfDaysOfMonths);
+  let startingIndex = 9;
+  let endingIndex = 3;
+  let counter = 0;
+  while (startingIndex !== endingIndex) {
+    console.log(arrayOfSubarrays[startingIndex]);
+    if (startingIndex == 11) {
+      startingIndex = 0;
+      counter++;
+    } else {
+      startingIndex++;
+    }
+    if (counter == 2) break;
+  }
 })();
