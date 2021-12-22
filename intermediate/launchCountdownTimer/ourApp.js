4b657cf6172cc6388572fd48decba43dae3725b3
Toan <tpkhuong@gmail.com>
2021-12-22 04:15:38 -0500

    loving the effort/process building algorithm for current date month and starting date month js

--- intermediate/launchCountdownTimer/ourApp.js
+++ intermediate/launchCountdownTimer/ourApp.js
@@ -79,7 +79,32 @@
     };
   }
 
-  function findIndexOfSubarray(arrInput, strFormOfMonth) {}
+  /**
+   * using Object.keys to just get keys which will be "Mar", "Apr" etc
+   * since we will be working with the same array the length will be the same
+   * we can use indexOf
+   * **/
+  function findIndexMonthUsingIndexOf(array, strFormMonth) {
+    return array.indexOf(strFormMonth);
+  }
+  /**
+   * Using Object.entries to get an arrays of subarray with [key,value]
+   * finding index using reduce where we pass in an array of subarrays
+   * each subarray will have month, numOfDays
+   *  ["Mar", 31]
+   * **/
+  function findIndexMonthInArrayUsingReduce(arrInput, strFormOfMonth) {
+    // passing in an array with just the month without the
+    // number of days
+    return arrInput.reduce(function findIndex(buildingUp, currentArray, index) {
+      // currentArray will be [month, numDays]
+      const [month, numOfDays] = currentArray;
+      if (month == strFormOfMonth) {
+        buildingUp.push(index);
+      }
+      return buildingUp;
+    }, []);
+  }
 
   /***
    * ex: Dec 20, 2021 starting date March 23,2022
@@ -107,19 +132,125 @@
     }
     if (counter == 2) return;
   }
-   * ***/
   const arrayOfSubarrays = Object.entries(dataObj.numOfDaysOfMonths);
-  let startingIndex = 9;
-  let endingIndex = 3;
-  let counter = 0;
-  while (startingIndex !== endingIndex) {
-    console.log(arrayOfSubarrays[startingIndex]);
-    if (startingIndex == 11) {
-      startingIndex = 0;
-      counter++;
-    } else {
-      startingIndex++;
+    let startingIndex = 9;
+    let endingIndex = 3;
+    let counter = 0;
+    while (startingIndex !== endingIndex) {
+      console.log(arrayOfSubarrays[startingIndex]);
+      if (startingIndex == 11) {
+        startingIndex = 0;
+        counter++;
+      } else {
+        startingIndex++;
+      }
+      if (counter == 2) break;
     }
-    if (counter == 2) break;
+   * ***/
+  const arrOfMonths = Object.keys(dataObj.numOfDaysOfMonths);
+  const arrayOfSubarrays = Object.entries(dataObj.numOfDaysOfMonths);
+  const currentMonth = findIndexMonthUsingIndexOf(arrOfMonths, "Dec");
+  const startingMonth = findIndexMonthUsingIndexOf(arrOfMonths, "Mar");
+
+  arrayNumsOfDaysInMonth(currentMonth, startingMonth + 1, arrayOfSubarrays);
+  /**
+   * have func that loop starting at currentMonth index and ends at startingMonth index
+   * while loop add the daysOfMonth of that month into array
+   * we will use first and last month values in array to calculate
+   * days in current month and days in starting month without have to access that value
+   * from dataObj
+   * **/
+  function arrayNumsOfDaysInMonth(startingIndex, endingIndex, arrOfSubarrays) {
+    // copy array
+    // const copyOfArray = arrOfSubarrays.slice();
+    // const copyOfArray = [].concat(arrOfSubarrays);
+    const copyOfArray = [...arrOfSubarrays];
+    const result = [];
+    let counter = 0;
+    while (startingIndex !== endingIndex) {
+      console.log(copyOfArray[startingIndex]);
+      const eachSubarray = copyOfArray[startingIndex];
+      const [month, daysOfMonth] = eachSubarray;
+      result.push(daysOfMonth);
+      if (startingIndex >= 11) {
+        startingIndex = 0;
+      } else {
+        startingIndex++;
+      }
+    }
+    console.log(result);
+    // const startingDateNumOfDays = result.pop();
+    const [lastValue, resultArrWithoutLastValue] =
+      getStartingDateNumOfDays(result);
+    console.log(lastValue);
+    console.log(resultArrWithoutLastValue);
+    // const [firstValue, ...rest] = resultArrWithoutLastValue;
+    // console.log(firstValue);
+    // console.log(rest);
+    const firstValue = getCurrentMonthNumOfDays(resultArrWithoutLastValue);
+    console.log(firstValue);
+    const daysBetweenCurrentAndStartingMonth =
+      numOfDaysBetweenCurrAndStartingMonth(resultArrWithoutLastValue);
+    console.log(daysBetweenCurrentAndStartingMonth);
+    console.log(
+      "total days",
+      totalDaysOfMonthsBetweenCurrentAndStartingDate(
+        daysBetweenCurrentAndStartingMonth
+      )
+    );
   }
+  /**
+   * helper funcs
+   * **/
+
+  /**
+   * starting month num of days
+   * **/
+
+  function getStartingDateNumOfDays(arrInput) {
+    const copyOfArr = [...arrInput];
+    const lastValueOfArr = copyOfArr.pop();
+    return [lastValueOfArr, copyOfArr];
+  }
+  /**
+   * get current month num of days
+   * **/
+
+  function getCurrentMonthNumOfDays(array) {
+    const copyOfArr = [...array];
+    const [firstValue] = copyOfArr;
+    return firstValue;
+  }
+
+  /**
+   * num of days between current and start month
+   * **/
+  function numOfDaysBetweenCurrAndStartingMonth(array) {
+    const copyOfArr = [...array];
+    const [, ...rest] = copyOfArr;
+    return rest;
+  }
+  /**
+   * get total days months between current and starting date
+   * **/
+
+  function totalDaysOfMonthsBetweenCurrentAndStartingDate(array) {
+    const copyOfArr = [...array];
+    return copyOfArr.reduce(function addValues(buildingUp, currentDays) {
+      return buildingUp + currentDays;
+    }, 0);
+  }
+
+  /**
+   * using slice
+   * 
+   function usingSliceToGetNumOfDays(array) {
+     const copyOfArray = [...array];
+     const lengthOfArr = copyOfArray.length;
+     const [firstValue] = copyOfArray.slice(0, 0 + 1);
+     const [betweenValues] = copyOfArray.slice(0 + 1, lengthOfArr - 1);
+     const [lastValue] = copyOfArray.slice(lengthOfArr - 1);
+     return [firstValue, betweenValues, lastValue];
+   }
+   * **/
 })();
